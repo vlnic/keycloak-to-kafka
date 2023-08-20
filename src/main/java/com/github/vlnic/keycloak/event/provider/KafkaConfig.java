b73934d01b62360c6e8b03eb2bd8cdefa14d8073
Vladimir Pavlov <vladimir.pavlov@ccsteam.ru>
2023-08-20 20:33:03 +0400

    added build producer method

--- src/main/java/com/github/vlnic/keycloak/event/provider/KafkaConfig.java
+++ src/main/java/com/github/vlnic/keycloak/event/provider/KafkaConfig.java
@@ -1,7 +1,61 @@
 package com.github.vlnic.keycloak.event.provider;
 
-public class KafkaConfig {
-    public KafkaConfig() {
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.keycloak.Config.Scope;
 
+import org.jboss.logging.Logger;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Locale;
+import java.util.Properties;
+
+public class KafkaConfig {
+
+    private static final Logger log = Logger.getLogger(KafkaEventListenerProvider.class);
+
+    private Properties properties;
+
+    private String topicName;
+
+    public KafkaConfig() {
+        Properties props = new Properties();
+        props.put("key.serializer", StringSerializer.class.getName());
+        props.put("value.serializer", StringSerializer.class.getName());
+        log.info("initialized KafkaConfig");
+        this.properties = props;
+    }
+
+    public static KafkaConfig createFromScope(Scope cnf) throws UnknownHostException {
+        KafkaConfig config = new KafkaConfig();
+        config.properties.put(
+                "client.id",
+                resolveConfigVar(cnf, "client_id", InetAddress.getLocalHost().getHostName())
+        );
+        config.properties.put(
+                "bootstrap.servers",
+                resolveConfigVar(cnf, "bootstrap_servers", "")
+        );
+        config.properties.put("acks", resolveConfigVar(cnf, "acks", "all"));
+
+        return config;
+    }
+
+    public static String resolveConfigVar(Scope config, String varName, String defaultValue) {
+        String value = defaultValue;
+        if (config != null && config.get(varName) != null) {
+            value = config.get(varName);
+        } else {
+            String envVariableName = "KK_TO_KAFKA_" + varName.toUpperCase(Locale.ENGLISH);
+            String env = System.getenv(envVariableName);
+            if (env != null) {
+                value = env;
+            }
+        }
+        return value;
+    }
+
+    public Properties getProperties() {
+        return properties;
     }
 }
--- src/main/java/com/github/vlnic/keycloak/event/provider/KafkaEventListenerProvider.java
+++ src/main/java/com/github/vlnic/keycloak/event/provider/KafkaEventListenerProvider.java
@@ -1,20 +1,37 @@
 package com.github.vlnic.keycloak.event.provider;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
+import org.jboss.logging.Logger;
 import org.keycloak.events.Event;
 import org.keycloak.events.EventListenerProvider;
+import org.keycloak.events.EventListenerTransaction;
 import org.keycloak.events.admin.AdminEvent;
+import org.keycloak.models.KeycloakSession;
 
 public class KafkaEventListenerProvider implements EventListenerProvider {
+
+    private final KafkaConfig cnf;
+
+    private static final Logger log = Logger.getLogger(KafkaEventListenerProvider.class);
+
+    private final EventListenerTransaction tx = new EventListenerTransaction(this::publishAdminEvent, this::publishEvent);
+
+    private KafkaProducer producer;
+
+    public KafkaEventListenerProvider(KafkaConfig config, KeycloakSession session) {
+        this.cnf = config;
+        this.producer = new KafkaProducer(config.getProperties());
+        session.getTransactionManager().enlistAfterCompletion(tx);
+    }
+
     @Override
     public void onEvent(Event event) {
-
+        tx.addEvent(event);
     }
 
     @Override
     public void onEvent(AdminEvent adminEvent, boolean b) {
-
+        tx.addAdminEvent(adminEvent, b);
     }
 
     @Override
@@ -22,6 +39,15 @@ public class KafkaEventListenerProvider implements EventListenerProvider {
 
     }
 
-    public void publishEvent() {
+    private void publishEvent(Event event) {
+        log.info("publish common event");
+    }
+
+    private void publishAdminEvent(AdminEvent adminEvent, boolean includeRepresentation) {
+        log.info("publish admin event");
+    }
+
+    public KafkaProducer getProducer() {
+        return producer;
     }
 }
